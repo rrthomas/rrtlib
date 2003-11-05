4b82dc9991a3da20776237296c6d13b838e26939
Reuben Thomas <rrt@sc3d.org>
2003-11-05 21:57:37 +0000

    Rewritten to be much simpler and with a simpler API. There are now fewer types (only one struct) and functions to deal with, and the API is easier to understand.
    I'll add some tests shortly.

--- hash.c
+++ hash.c
@@ -1,128 +1,106 @@
+/* Open hash tables */
+
 #include <stdlib.h>
 #include <string.h>
+
+#include "memory.h"
 #include "except.h"
 #include "hash.h"
 
+
+/* Create a hash table of size size with a hashing function hash and
+   comparison function compare; a pointer to the table is returned */
 HashTable *
-hashNew(size_t size, Hasher hash, Comparer compare)
+hash_new(size_t size, Hasher hash, HashComparer compare)
 {
-    HashTable *table= new(HashTable);
+  HashTable *table = new(HashTable);
 
-    table->thread= excCalloc(size, sizeof(HashNode *));
-    table->size= size; table->hash= hash;  table->compare= compare;
-    return table;
+  table->chain = exc_calloc(size, sizeof(HashNode));
+  table->size = size;
+  table->hash = hash;
+  table->compare = compare;
+
+  return table;
 }
 
+/* Free a hash table */
 void
-hashDestroy (HashTable *table)
+hash_free (HashTable *table)
 {
-    size_t i;
-    HashNode *p, *q;
+  HashNode *c, *p, *q, *end = table->chain + table->size;
 
-    for (i= 0; i < table->size; i++)
-        for (p= table->thread[i]; p != NULL; p= q) {
-            q= p->next;
-            free(p->key);
-            free(p->body);
-            free(p);
-        }
-    free(table->thread);
-    free(table);
-}
-
-typedef struct {
-    size_t entry;
-    HashNode *prev;
-    HashNode *curr;
-    int found;
-} HashLink;
-
-static HashLink
-hashSearch(HashTable *table, void *key)
-{
-    size_t entry= table->hash(key) % table->size;
-    HashLink ret;
-
-    ret.entry= entry;
-    ret.prev= NULL;
-    ret.curr= table->thread[entry];
-    ret.found= HASH_NOTFOUND;
-
-    while (ret.curr != NULL) {
-        if (table->compare(key, ret.curr->key)) {
-            ret.found= HASH_FOUND;
-            return ret;
-        }
-        ret.prev= ret.curr;
-        ret.curr= ret.curr->next;
+  for (c = table->chain; c < end; c++)
+    for (p = c->next; p != NULL; p = q) {
+      q = p->next;
+      free(p->key);
+      free(p->data);
+      free(p);
     }
-
-    return ret;
+  free(table->chain);
+  free(table);
 }
 
-void *
-hashFind(HashTable *table, void *key)
+/* Search a hash table for an object with key key, returning a pointer
+   to the HashNode, or the start of the chain in which it would go if
+   there is no such key. */
+HashNode *
+hash_find(HashTable *table, void *key)
 {
-    HashLink l= hashSearch(table, key);
+  HashNode *start = table->chain + table->hash(key) % table->size;
+  HashNode *curr;
 
-    if (l.found == HASH_NOTFOUND) return NULL;
+  for (curr = start;
+       curr != NULL && !table->compare(key, curr->key);
+       curr = curr->next);
 
-    return l.curr->body;
+  return curr != NULL ? curr : start;
 }
 
-void *
-hashInsert(HashTable *table, void *key, void *body)
+/* Set key key in a hash table to object data. Return the previous
+   data for that key. */
+void
+hash_set(HashTable *table, void *key, void *data)
 {
-    HashLink l= hashSearch(table, key);
-    HashNode *n;
+  HashNode *node = hash_find(table, key);
 
-    if (l.found == HASH_FOUND) return l.curr->body;
-
-    n= new(HashNode);
-
-    if (l.prev == NULL) table->thread[l.entry]= n;
-    else l.prev->next= n;
-
-    n->next= l.curr;  n->key= key; n->body= body;
-
-    return NULL;
+  if (node->data != NULL) {
+    free(node->data);
+    if (data != NULL)
+      node->data = data;
+    else {
+      free(node->key);
+      free(node);
+    }
+  } else if (data) {
+    HashNode *new = new(HashNode);
+    node->next = new;
+    new->key = key;
+    new->data = data;
+  }
 }
 
-int
-hashRemove(HashTable *table, void *key)
-{
-    HashLink l= hashSearch(table, key);
 
-    if (l.found == HASH_NOTFOUND) return HASH_NOTFOUND;
+/* Hasher and HashComparer functions for string keys */
 
-    if (l.prev == NULL) table->thread[l.entry]= l.curr->next;
-    else l.prev->next= l.curr->next;
-
-    free(l.curr->key);
-    free(l.curr->body);
-    free(l.curr);
-
-    return HASH_OK;
-}
-
+/* Adapted from the Red Dragon book */
 size_t
-hashStrHash(void *str)
+hash_strhash(void *str)
 {
-    char *p, *s= (char *)str;
-    unsigned long h= 0, g;
+  char *p, *s = (char *)str;
+  unsigned long h = 0, g;
 
-    for (p= s; *p != '\0'; p++) {
-        h= (h << 4) + *p;
-        if ((g= h & 0xf0000000)) {
-            h ^= (g >> 24);
-            h ^= g;
-        }
+  for (p = s; *p != '\0' && (p - s) < 32; p++) {
+    h = (h << 4) + *p;
+    if ((g = h & 0xf0000000)) {
+      h ^= (g >> 24);
+      h ^= g;
     }
+  }
 
-    return h;
+  return h;
 }
 
-int hashStrcmp(void *s, void *t)
+bool hash_strcmp(void *s, void *t)
 {
-    return (strcmp((char *)s, (char *)t) == 0);
+  return strcmp((char *)s, (char *)t) == 0;
 }
--- hash.h
+++ hash.h
@@ -1,36 +1,46 @@
+/* Open hash tables */
+
 #ifndef HASH_H
 #define HASH_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
+/* Hash tables are constructed from chains of HashNodes, which contain
+   a link to the next node, and pointers to the key and body of the
+   object. */
 typedef struct HashNode HashNode;
 struct HashNode {
   HashNode *next;
   void *key;
-  void *body;
+  void *data;
 };
 
+/* The hashing function type */
 typedef size_t (*Hasher)(void *);
-typedef int (*Comparer)(void *, void *);
 
+/* The comparison function type */
+typedef bool (*HashComparer)(void *, void *);
+
+/* A hash table consists of a pointer to an array of HashNodes, the
+   size of the array, a hashing function and a comparison function. */
 typedef struct {
-    HashNode **thread;
-    size_t size;
-    Hasher hash;
-    Comparer compare;
+  HashNode *chain;
+  size_t size;
+  Hasher hash;
+  HashComparer compare;
 } HashTable;
 
-HashTable *hashNew(size_t size, Hasher hash, Comparer compare);
-void hashDestroy(HashTable *table);
-void *hashFind(HashTable *table, void *key);
-void *hashInsert(HashTable *table, void *key, void *body);
-int hashRemove(HashTable *table, void *key);
+HashTable *hash_new(size_t size, Hasher hash, HashComparer compare);
+void hash_free(HashTable *table);
+HashNode *hash_find(HashTable *table, void *key);
+void hash_set(HashTable *table, void *key, void *body);
 
-#define HASH_OK 0
-#define HASH_FOUND 1
-#define HASH_NOTFOUND 2
+/* Return the object associated with the given key */
+#define hash_get(table, key) \
+  hash_find((table), (key)).curr->data
 
-size_t hashStrHash(void *);
-int hashStrcmp(void *, void *);
+size_t hash_strhash(void *);
+bool hash_strcmp(void *, void *);
 
 #endif
