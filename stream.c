78ae18ec377923e2f525e1294e514669ebfc4a94
Reuben Thomas <rrt@sc3d.org>
2004-02-11 17:55:52 +0000

    Reformat code to my currently preferred format and start adding comments based on the .w file.

--- stream.c
+++ stream.c
@@ -1,48 +1,61 @@
+/* Stream utilities */
+
 #include <stdio.h>
 
 #include "except.h"
+#include "memory.h"
 #include "stream.h"
 
+
+/* Find the length of the given stream, which must be seekable;
+   returns -1 if there is an error */
 long
 flen(FILE *fp)
 {
-    long pos= ftell(fp);
+  long pos = ftell(fp);
 
-    if (pos != -1 && fseek(fp, 0, SEEK_END) == 0) {
-        long len= ftell(fp);
+  if (pos != -1 && fseek(fp, 0, SEEK_END) == 0) {
+    long len = ftell(fp);
 
-        if (len != -1 && fseek(fp, pos, SEEK_SET) == 0)
-            return len;
-    }
-    return -1;
+    if (len != -1 && fseek(fp, pos, SEEK_SET) == 0)
+      return len;
+  }
+
+  return -1;
 }
 
+/* Read up to the first size bytes of file into the buffer pointed to
+   by buf, opening it with mode mode. Returns the number of bytes
+   actually read. If there is an error opening or reading the file, an
+   exception is thrown.*/
 long
-slurp(const char *file, const char *mode, char **buf, long size)
+slurp(const char *file, const char *mode, long size, char *buf)
 {
-    FILE *fp= fopen(excFile, mode);
+  FILE *fp = fopen(file, mode);
 
-    if (!fp) throw("could not open file");
-    if (*buf == NULL) *buf= excMalloc(size);
-    else if ((size= flen(fp)) < 0) throw("error getting length of file");
-    if (size == 0) return 0;
-    fread(*buf, sizeof(char), size, fp);
-    if (ferror(fp)) throw("error reading file");
-    fclose(fp);
-    return size;
+  if (!fp)
+    throw("slurp could not open file");
+  fread(buf, sizeof(char), size, fp);
+  if (ferror(fp))
+    throw("error slurping file");
+  fclose(fp);
+
+  return size;
 }
 
 char *
-getLn(FILE *fp)
+getln(FILE *fp)
 {
-    size_t len= 256;
-    int c;
-    char *l= excMalloc(len), *s= l;
+  size_t len = 256;
+  int c;
+  char *l = exc_malloc(len), *s = l;
 
-    for (c= getc(fp); c != '\n' && c != EOF; c= getc(fp)) {
-        if (s == l + len) excRealloc(l, len *= 2);
-        *s++= c;
-    }
-    *s= '\0';
-    return l;
+  for (c = getc(fp); c != '\n' && c != EOF; c = getc(fp)) {
+    if (s == l + len)
+      exc_realloc(l, len *= 2);
+    *s++ = c;
+  }
+  *s = '\0';
+
+  return l;
 }
--- stream.h
+++ stream.h
@@ -1,10 +1,12 @@
+/* Stream utilities */
+
 #ifndef STREAM_H
 #define STREAM_H
 
 #include <stdio.h>
 
 long flen(FILE *fp);
-long slurp(const char *file, const char *mode, char **buf, long size);
-char *getLn(FILE *fp);
+long slurp(const char *file, const char *mode, long size, char *buf);
+char *getln(FILE *fp);
 
 #endif
