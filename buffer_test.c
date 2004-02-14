4564a4f7ff189a9ae7b818ab8ffb46feb1e358e2
Reuben Thomas <rrt@sc3d.org>
2004-02-14 11:24:03 +0000

    Update tests to match updated modules.

--- buffer_test.c
+++ buffer_test.c
@@ -12,30 +12,20 @@ char *progname = "buffer_test";
 
 int main(void)
 {
-  Buffer *b = buf_new(16);
+  Buffer *b = buf_new();
 
   /* Check buffer's size and number of bytes used */
-  assert(b->used == 0);
-  assert(b->size == 16);
-
-  /* Grow buffer and check size */
-  buf_realloc(b, b->size + 1);
-  assert(b->size > 16);
-
-  /* Shrink buffer and check size */
-  buf_realloc(b, 8);
-  assert(b->size == 8);
+  assert(b->items == 0);
+  assert(b->size == 0);
 
   /* Add a byte to the buffer and check it and the buffer's size */
   buf_add(b, uint8_t, 27);
-  assert(*(uint8_t *)(b->data) == 27);
-  assert(b->size == 8);
+  assert(*(uint8_t *)(b->array) == 27);
+  assert(b->items == 1);
 
   /* buf_add a word, having buf_align'ed */
 
   /* buf_add a block of data */
-
-  /* test size after buf_realloc(b,0) */
 
   /* Free the buffer */
   buf_free(b);
--- vector_test.c
+++ vector_test.c
@@ -15,27 +15,12 @@ int main(void)
   Vector *b = vec_new(16);
 
   /* Check vector's size and number of bytes used */
-  assert(b->used == 0);
-  assert(b->size == 16);
+  assert(b->items == 0);
+  assert(b->itemsize == 16);
 
   /* Grow vector and check size */
-  vec_realloc(b, b->size + 1);
-  assert(b->size > 16);
-
-  /* Shrink vector and check size */
-  vec_realloc(b, 8);
-  assert(b->size == 8);
-
-  /* Add a byte to the vector and check it and the vector's size */
-  vec_add(b, uint8_t, 27);
-  assert(*(uint8_t *)(b->data) == 27);
-  assert(b->size == 8);
-
-  /* vec_add a word, having vec_align'ed */
-
-  /* vec_add a block of data */
-
-  /* test size after vec_realloc(b,0) */
+/*   vec_realloc(b, b->itemsize + 1); */
+/*   assert(b->itemsize > 16); */
 
   /* Free the vector */
   vec_free(b);
